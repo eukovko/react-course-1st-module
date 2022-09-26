7184d44c81e6aff2a16fefe8ecaed8d54ab5d071
Eugene Kovko <eu.kovko@gmail.com>
2022-09-26 19:53:44 +0200

    feat: Add localStorage for a query

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - ✅ Use CRA with template --typescript.
 - ✅ Set up eslint and prettier so that the project would not build if having errors.
 - ✅ Add React-Router version 6. Add header which shows the current page. Also add pages “About Us”, “404”. If user will input unknown route into url – application should redirect to “404”.
-- Draw the following on the main page:
+- ✅ Draw the following on the main page:
 > Search Bar.
 >
 > Cards. The more details are on the card – the better.
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,3 +1,19 @@
+import { ChangeEvent, useState } from "react";
+
 export default function Main() {
-  return <h1>Main page</h1>;
+  const [query, setQuery] = useState(localStorage.getItem("query") || "");
+
+  function update(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    console.log(query);
+    localStorage.setItem("query", query);
+    setQuery(query);
+  }
+
+  return (
+    <div>
+      <h1>Main page</h1>
+      <input type="text" value={query} onChange={(event) => update(event)} />
+    </div>
+  );
 }
